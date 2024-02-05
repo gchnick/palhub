c39669fc2eb850e852591789fa46623e802a1de6
Nick Galan <gnick.alan@gmail.com>
2024-02-05 08:39:17 -0400

    chore(app)::wrench: remove monorepo nx

--- apps/service-hub-e2e/src/e2e/app.component.cy.ts
+++
@@ -1,6 +0,0 @@
-describe('service-hub', () => {
-  beforeEach(() => cy.visit('/iframe.html?id=appcomponent--primary'));
-  it('should render the component', () => {
-    cy.get('api-root').should('exist');
-  });
-});
